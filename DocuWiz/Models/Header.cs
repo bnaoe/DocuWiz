6c7e3a3dc0b0835d928e0a2a1d94b97b5e68cd46
bnaoe <bp.naoe@gmail.com>
2021-08-17 22:24:34 -0500

    Add project files.

---
+++ DocuWiz/Models/Header.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocuWiz.Models
+{
+    public class Header
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
